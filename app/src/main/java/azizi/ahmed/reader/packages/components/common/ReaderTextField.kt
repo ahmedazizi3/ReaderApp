ece582f58020850e74b59b6dc8672ab71ccb6c3a
DELL <aziziahmed027@gmail.com>
2025-03-01 15:00:01 +0100

    feat: Create reusable ReaderTextField component
    This commit introduces the `ReaderTextField` composable, a reusable component for handling text input in the application.
    
    **Key changes:**
    
    -   **ReaderTextField.kt:**
        -   Created `ReaderTextField` as a composable function.
        -   Implemented `OutlinedTextField` with customizable properties:
            -   `textFieldValue`: A `MutableState<String>` to manage the text input value.
            -   `readerLabel`: A `String` for the label displayed above the text field.
            -   `isSingleLine`: A `Boolean` to specify if the text field should be single-line or multi-line.
            -   `keyboardOptions`: `KeyboardOptions` to configure the keyboard type.
            - `visualTransformation`: `VisualTransformation` to transform the text visually.
        -   Set default colors for focused/unfocused states:
            -   Container color: White
            -   Indicator color: Light Gray
            -   Text color: Light Gray
            -   Cursor color: Light Gray
            -   Label color: Light Gray
        -   Added padding to the text field for visual spacing.
        -   Filled the width.

---
+++ app/src/main/java/azizi/ahmed/reader/packages/components/common/ReaderTextField.kt
@@ -0,0 +1,58 @@
+package azizi.ahmed.reader.packages.components.common
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ReaderTextField(
+    modifier: Modifier = Modifier,
+    textFieldValue: MutableState<String>,
+    readerLabel: String,
+    isSingleLine: Boolean,
+    keyboardOptions: KeyboardOptions,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+
+    OutlinedTextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        value = textFieldValue.value,
+        onValueChange = {
+            textFieldValue.value = it
+        },
+        label = {
+            Text(
+                text = readerLabel
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            focusedIndicatorColor = Color.LightGray,
+            unfocusedIndicatorColor = Color.LightGray,
+            focusedTextColor = Color.LightGray,
+            cursorColor = Color.LightGray,
+            unfocusedLabelColor = Color.LightGray,
+            focusedLabelColor = Color.LightGray,
+            unfocusedTextColor = Color.LightGray,
+
+        ),
+        singleLine = isSingleLine,
+        keyboardOptions = keyboardOptions,
+        visualTransformation = visualTransformation
+    )
+}
