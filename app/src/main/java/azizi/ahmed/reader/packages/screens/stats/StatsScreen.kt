134f8c7f32273ba7186f26cfec2dd6da9c69556b
DELL <aziziahmed027@gmail.com>
2025-06-14 22:02:46 +0100

    feat: Implement basic StatsScreen UI
    This commit implements a basic UI for the StatsScreen.
    
    **Key changes:**
    
    -   **StatsScreen.kt:**
        -   Added a `Column` composable to arrange elements vertically.
        -   Centered the content horizontally and vertically within the screen.
        -   Set the background color to white.
        -   Displayed a `Text` composable with the text "Stats Screen" in black, 30sp font size, and bold font weight.

--- app/src/main/java/azizi/ahmed/reader/packages/screens/stats/StatsScreen.kt
+++ app/src/main/java/azizi/ahmed/reader/packages/screens/stats/StatsScreen.kt
@@ -1,11 +1,35 @@
 package azizi.ahmed.reader.packages.screens.stats
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun StatsScreen(
     modifier: Modifier = Modifier
 ) {
-
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                color = Color.White
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Stats Screen",
+            color = Color.Black,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
