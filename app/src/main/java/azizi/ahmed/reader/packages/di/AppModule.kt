4f8cbcf5e75f2bc35eaecd0f21d57d9e7af0564a
DELL <aziziahmed027@gmail.com>
2025-06-17 10:34:14 +0100

    feat: Implement Hilt AppModule for dependency injection
    This commit introduces the `AppModule` using Dagger Hilt for providing application-level dependencies.
    
    **Key changes:**
    
    -   **AppModule.kt:**
        -   Annotated with `@Module` and `@InstallIn(SingletonComponent::class)` to make bindings available in the application component.
        -   Provides a singleton instance of `BooksAPI` configured with Retrofit:
            -   Sets the `BASE_URL`.
            -   Adds `GsonConverterFactory` for JSON serialization/deserialization.
        -   Provides a singleton instance of `BooksRepository`, injecting the `BooksAPI` dependency.

--- app/src/main/java/azizi/ahmed/reader/packages/di/AppModule.kt
+++ app/src/main/java/azizi/ahmed/reader/packages/di/AppModule.kt
@@ -1,11 +1,33 @@
 package azizi.ahmed.reader.packages.di
 
+import azizi.ahmed.reader.packages.network.BooksAPI
+import azizi.ahmed.reader.packages.repository.BooksRepository
+import azizi.ahmed.reader.packages.utils.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideBooksRepository(booksAPI: BooksAPI) = BooksRepository(booksAPI)
+
+    @Singleton
+    @Provides
+    fun provideBookAPI(): BooksAPI {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(BooksAPI::class.java)
+    }
 
 }
