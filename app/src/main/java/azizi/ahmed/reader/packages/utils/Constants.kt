813c8e6445871ca799c1b4703f39a21177094ff8
DELL <aziziahmed027@gmail.com>
2025-06-17 10:37:23 +0100

    feat: Add BASE_URL constant for Google Books API
    This commit introduces a `Constants.kt` file and defines the `BASE_URL` for the Google Books API.
    
    The `BASE_URL` is set to `https://www.googleapis.com/books/v1/`.

---
+++ app/src/main/java/azizi/ahmed/reader/packages/utils/Constants.kt
@@ -0,0 +1,6 @@
+package azizi.ahmed.reader.packages.utils
+
+object Constants {
+//    https://www.googleapis.com/books/v1/volumes?q=android
+    const val BASE_URL = "https://www.googleapis.com/books/v1/"
+}
